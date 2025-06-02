0ec1457e69222467b0a3507337404a35ff206e80
SushantChoudhary-iitr <sushant_c@ce.iitr.ac.in>
2025-06-03 01:33:04 +0530

    upadted cors for netlify server

--- server/server.js
+++ server/server.js
@@ -24,8 +24,23 @@ app.use(session({
 app.use(express.json());
 
 // Enable CORS for frontend
-app.use(cors({
+/*app.use(cors({
   origin: 'http://localhost:5173', // Vite's default port
+  credentials: true
+}));*/
+const allowedOrigins = [
+  'http://localhost:5173',              // for local dev
+  'https://my-app.netlify.app'          // for production Netlify frontend
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
